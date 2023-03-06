332663c427417679af0538c3216c848beb9b2bbd
Neal A <73256760+nealarch01@users.noreply.github.com>
2023-03-06 13:12:05 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('./src/index.html')
+}
+
+app.whenReady().then(() => {
+    console.log(process.version);
+    createWindow()
+});
