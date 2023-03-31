a3b0125cd7531623f23c1056b7ffd273043c6548
Neal A <73256760+nealarch01@users.noreply.github.com>
2023-03-30 22:47:31 -0700

    Fix start page redirection and window start size

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+const isDev = true;
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1000,
-        height: 800,
+        height: 650,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     });
-    win.loadFile('./src/views/html/homepage.html')
+    win.loadFile('./src/views/html/control_panel.html')
+    if (isDev) {
+        win.webContents.openDevTools()
+    }
 }
 
 app.whenReady().then(() => {
