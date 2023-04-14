cc1c467e124f41c1286fe24478f1fdfda5e32fe6
Neal A <73256760+nealarch01@users.noreply.github.com>
2023-04-14 16:04:01 -0700

    Update team_model.js and constructor
    Remove default array initialization. Add parameter to set members

--- src/models/team_model.js
+++ src/models/team_model.js
@@ -1,19 +1,10 @@
-// import { Person } from "./person_model.js";
-// import { Stats } from "./stats_model.js";
-// import { Configs } from "./configs_model.js";
-
 const { Person } = require("./person_model");
 const { Stats } = require("./stats_model");
-const { Configs } = require("./configs_model");
 
 class Team {
-    constructor() {
+    constructor(name, members) {
         this.name = "";
-        this.members = [ // Including active and inactive members
-            new Person("Neal"),
-            new Person("Irvin"),
-            new Person("Simon")
-        ];
+        this.members = members;
     }
 }
 
