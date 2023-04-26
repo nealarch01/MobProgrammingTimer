0d769b70ddc2b7db2916faaadca63fd651acc1d6
Neal A <73256760+nealarch01@users.noreply.github.com>
2023-04-26 12:00:55 -0700

    Fix write to file

--- src/controllers/team_controller.js
+++ src/controllers/team_controller.js
@@ -27,14 +27,14 @@ class TeamController {
         const jsonData = storage.getSync('mock_teams'); //TODO: change to team configs
         this.activeQueue = [];
         this.inactiveQueue = [];
-        this.currentTeamIndex = jsonData.last_team_index ?? -1;
+        this.currentTeamIndex = jsonData.lastTeamIndex ?? -1;
         this.allTeams = jsonData.teams ?? [];
     }
 
     writeFile() {
-        let filepath = path.join(__dirname, "../../configs/mock_teams.json"); //TODO: MOVE TO mock_teams*********
+        let filepath = path.join(__dirname, "../../configs/mock_teams.json"); //TODO: Change to teams.json
         const jsonData = {
-            "last_team_index": this.currentTeamIndex,
+            "lastTeamIndex": this.currentTeamIndex,
             "teams": this.allTeams
         }
         fs.writeFile(filepath, JSON.stringify(jsonData, null, 4), "utf8", (err) => {
@@ -47,7 +47,7 @@ class TeamController {
             this.noneTeam.timerConfig = timerConfig;
             return;
         }
-        this.allTeams[this.currentTeamIndex].timerConfigs = timerConfig;
+        this.allTeams[this.currentTeamIndex].timerConfig = timerConfig;
         this.writeFile();
     }
 
